10bfdcacd418e96f5097091cedf89a95718a82e3
mateusnovo <mateus.novo@copelmi.com.br>
2026-03-23 19:43:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rede-srt-rs',
+  webDir: 'www'
+};
+
+export default config;
